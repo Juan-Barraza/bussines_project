f4bef068aa0ef8fc4a5ee16df3ae585f549b0f50
Juan Barraza <juanbarrraza2004@outlook.com>
2024-10-04 12:52:08 -0500

    add home.html and others modifications

--- scrips/main.js
+++ scrips/main.js
@@ -46,7 +46,7 @@ if (loginForm) {
         sessionStorage.setItem('activeUser', JSON.stringify(user));
 
         // Redirect
-        window.location.href = 'index.html';
+        window.location.href = 'home.html';
       } else {
         alert('password incorrecta');
       }
@@ -61,14 +61,21 @@ window.addEventListener('load', function() {
   const activeUser = JSON.parse(sessionStorage.getItem('activeUser'));
   console.log('User logged in:', activeUser);
 
+  if (window.location.pathname.includes('home.html') && !activeUser) {
+    // user active redirect login
+    window.location.href = 'login.html';
+    return;
+  }
+
+
   // Select elements
-  const navLinks = document.getElementById('nav-links');
-  const registerForm = document.getElementById('registerForm');
-  const containerLogin = document.querySelector('.containerLogin');
+  const navLinks = document.getElementById('nav-links-2');
+  // const registerForm = document.getElementById('registerForm');
+  // const containerLogin = document.querySelector('.containerLogin');
   
   if (activeUser) {
     const userItem = document.createElement('li');
-    userItem.textContent = `${activeUser.name} ${activeUser.lastName}`; 
+    userItem.textContent = activeUser.name ; 
     navLinks.appendChild(userItem);
     
     // Element to log out 
@@ -77,25 +84,21 @@ window.addEventListener('load', function() {
     logoutLink.href = '#';
     logoutLink.textContent = 'Log out';
 
-    const elementsToHide = document.getElementsByClassName("active-hide");
-    console.log(elementsToHide);
+    // const elementsToHide = document.getElementsByClassName("active-hide");
+    // console.log(elementsToHide);
 
     // hide login and register 
-    for (const e of elementsToHide) {
-      e.style.display = "none";
-    }
+    // for (const e of elementsToHide) {
+    //   e.style.display = "none";
+    // }
 
     logoutLink.addEventListener('click', function() {
       // Remove user of sessionStorage
       sessionStorage.removeItem('activeUser');
-
-      // Remove elements from nav
-      navLinks.removeChild(userItem);
-      navLinks.removeChild(logoutItem);
-
-
+      window.location.href = 'index.html'
+  
       // Reload page to reset
-      window.location.reload();
+      //window.location.reload();
     });
 
     logoutItem.appendChild(logoutLink);
